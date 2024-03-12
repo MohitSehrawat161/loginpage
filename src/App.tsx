66e1620fa43e8bae433e87780d6d4a95797b97a8
MohitSehrawat161 <mohit.sehrawat@binaryinformatics.com>
2024-03-12 12:12:44 +0530

    signup page ui

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import 'devextreme/dist/css/dx.light.css';
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
+import SignupPage  from './components/SignupPage';
 
 function App() {
   return (
     <div className="App">
+      <SignupPage/>
     
     </div>
   );
