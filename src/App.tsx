b7c3959dbe2886d8eb4da24aa4402292c8005e59
MohitSehrawat161 <mohit.sehrawat@binaryinformatics.com>
2024-03-12 11:46:42 +0530

    this commit code step

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import 'devextreme/dist/css/dx.light.css';
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+    
+    </div>
+  );
+}
+
+export default App;
